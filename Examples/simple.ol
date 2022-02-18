f7f5c6dc6296b687ca5dde312a22deaea5bf2700
Desmond Ainotohi <ntdesmond@gmail.com>
2022-02-18 17:55:57 +0300

    Fix variable declaration (use colon instead of assignment)

--- Examples/simple.ol
+++ Examples/simple.ol
@@ -1,6 +1,6 @@
 class Main is
   this is
-    var numbers := Array(3)
+    var numbers : Array(3)
     numbers.set(0, 5.15)
     numbers.set(1, 22.05)
     numbers.set(2, 15.22)
@@ -10,7 +10,7 @@ end
 
 class Calculator is
   method sum(numbers: Array) is
-    var result := T(0)
+    var result : T(0)
     i := 0
     while i.Less(numbers.Length) loop
       result := result.Plus(numbers.get(i))
--- Source/OCompiler/Analyze/Syntax/Declaration/Variable.cs
+++ Source/OCompiler/Analyze/Syntax/Declaration/Variable.cs
@@ -5,8 +5,11 @@ using OCompiler.Utils;
 
 namespace OCompiler.Analyze.Syntax.Declaration;
 
-internal class Variable: Assignment
+internal class Variable : BodyStatement
 {
+    public Identifier Identifier { get; }
+    public Expression.Expression Expression { get; }
+
     public static bool TryParse(TokenEnumerator tokens, out Variable? variable)
     {
         // Keyword.
@@ -15,24 +18,45 @@ internal class Variable: Assignment
             variable = null;
             return false;
         }
-        
-        // Get next token.
-        tokens.Next();
-        
-        // Parse statement.
-        if (!Assignment.TryParse(tokens, out Assignment? assignment))
+
+        // Parse name.
+        if (tokens.Next() is not Identifier name)
         {
-            throw new Exception($"Expected assignment name at position {tokens.Current().StartOffset}.");
+            throw new Exception($"Expected variable name at position {tokens.Current().StartOffset}.");
+        }
+        
+        // Assign delimiter.
+        if (tokens.Next() is not Lexical.Tokens.Delimiters.Colon)
+        {
+            throw new Exception($"Expected expression at position {tokens.Current().StartOffset}.");
         }
 
-        variable = new Variable(assignment!.Identifier, assignment.Expression);
+        // Get next token.
+        tokens.Next();
+
+        // Expression.
+        if (!Declaration.Expression.Expression.TryParse(tokens, out Expression.Expression? expression))
+        {
+            throw new Exception($"Expected expression at position {tokens.Current().StartOffset}.");
+        }
+
+        variable = new Variable(name, expression!);
         return true;
     }
 
-    protected Variable(Identifier name, Expression.Expression expression): base(name, expression){ } 
-    
+    protected Variable(Identifier name, Expression.Expression expression)
+    {
+        Identifier = name;
+        Expression = expression;
+    }
+
+    public override string ToString(string _)
+    {
+        return ToString();
+    }
+
     public override string ToString()
     {
-        return $"var {base.ToString()}";
+        return $"var {Identifier.Literal} : {Expression}";
     }
 }
