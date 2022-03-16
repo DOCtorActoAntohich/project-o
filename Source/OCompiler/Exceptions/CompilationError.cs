7e74b32c8935e6696189b8adfd2362eaaa74b73f
Desmond Ainotohi <ntdesmond@gmail.com>
2022-03-16 23:53:00 +0300

    moar custom errors

---
+++ Source/OCompiler/Exceptions/CompilationError.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using OCompiler.Analyze.Lexical;
+
+namespace OCompiler.Exceptions;
+
+internal class CompilationError : Exception
+{
+    public CompilationError(string message) : base(message) { }
+    public CompilationError(TokenPosition position, string message) : base($"{position}: {message}") { }
+}
+
