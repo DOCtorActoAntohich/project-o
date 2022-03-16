43211032516cbb8dc6cc80413676bd389432c172
DOCtorActoAntohich <fairy666death@mail.ru>
2022-03-16 17:42:16 +0300

    Add field generation for parsed classes

--- Source/OCompiler/CodeGeneration/CompileUnit.cs
+++ Source/OCompiler/CodeGeneration/CompileUnit.cs
@@ -1,8 +1,11 @@
 using System;
 using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
 using OCompiler.Analyze.Semantics;
 using OCompiler.Analyze.Semantics.Class;
-
+using OCompiler.Analyze.Semantics.Expression;
+using OCompiler.Analyze.Syntax.Declaration.Expression;
 using DomAnyRef = OCompiler.StandardLibrary.CodeDom.Reference.AnyRef;
 using DomIO     = OCompiler.StandardLibrary.CodeDom.Reference.IO;
 using DomString = OCompiler.StandardLibrary.CodeDom.Reference.String;
@@ -76,6 +79,18 @@ internal static class CompileUnit
         @namespace.Types.Add(typeDeclaration);
     }
 
+    private static void AddBaseClass(this CodeTypeDeclaration typeDeclaration, ClassInfo classInfo)
+    {
+        var parent = classInfo.BaseClass switch
+        {
+            ClassInfo parentInfo => new CodeTypeReference(parentInfo.Name),
+            Type type            => new CodeTypeReference(type),
+            _ => new CodeTypeReference(typeof(object))
+        };
+        
+        typeDeclaration.BaseTypes.Add(parent);
+    }
+
     private static CodeTypeDeclaration GetBuiltClass(BuiltClassInfo builtClassInfo)
     {
         return builtClassInfo.Name switch
@@ -93,20 +108,72 @@ internal static class CompileUnit
         };
     }
 
-    private static void AddBaseClass(this CodeTypeDeclaration typeDeclaration, ClassInfo classInfo)
-    {
-        var parent = classInfo.BaseClass switch
-        {
-            ClassInfo parentInfo => new CodeTypeReference(parentInfo.Name),
-            Type type => new CodeTypeReference(type),
-            _ => new CodeTypeReference(typeof(object))
-        };
-        
-        typeDeclaration.BaseTypes.Add(parent);
-    }
-
     private static void AddParsedClassContents(this CodeTypeDeclaration typeDeclaration, ParsedClassInfo classInfo)
     {
+        foreach (var field in classInfo.Fields)
+        { 
+            typeDeclaration.AddParsedClassFields(field);
+        }
+    }
+
+    private static void AddParsedClassFields(this CodeTypeDeclaration typeDeclaration, ParsedFieldInfo parsedField)
+    {
+        var field = new CodeMemberField
+        {
+            Name = parsedField.Name,
+            Type = new CodeTypeReference(parsedField.Type),
+            Attributes = MemberAttributes.Public,
+            InitExpression = ParseVariableInitExpression(parsedField.Expression.Expression)
+        };
         
+        typeDeclaration.Members.Add(field);
+    }
+
+    private static CodeExpression ParseVariableInitExpression(Expression expression)
+    {
+        return expression switch
+        {
+            Call call => ParseConstructorCall(call),
+            not null => new CodePrimitiveExpression(expression.Token),
+            _ => throw new Exception("Uncool init expression XCG1")
+        };
+    }
+
+    private static CodeExpression ParseConstructorCall(Call call)
+    {
+        var type = new CodeTypeReference(call.Token.Literal);
+        var arguments = ParseCallArgumentList(call.Arguments);
+
+        var ctorCall = new CodeObjectCreateExpression(type, arguments);
+
+        var chainedAccess = ParseChainAccess(ctorCall, call.Child);
+        return chainedAccess;
+    }
+
+    private static CodeExpression[] ParseCallArgumentList(IEnumerable<Expression> arguments)
+    {
+        return arguments.Select(ParseVariableInitExpression).ToArray();
+    }
+
+    private static CodeExpression ParseChainAccess(CodeExpression starterObject, Expression? expression)
+    {
+        switch (expression)
+        {
+            case null:
+                return starterObject;
+            
+            case Call call:
+            {
+                var objectAfterCall = new CodeMethodInvokeExpression(
+                    starterObject, call.Token.Literal, ParseCallArgumentList(call.Arguments));
+                return ParseChainAccess(objectAfterCall, call.Child);
+            }
+
+            default:
+            {
+                var accessedField = new CodeFieldReferenceExpression(starterObject, expression.Token.Literal);
+                return ParseChainAccess(accessedField, expression.Child);
+            }
+        }
     }
 }
