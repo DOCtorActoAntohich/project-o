299943e5cace4370120134b6f8ba031be5512194
Desmond Ainotohi <29694249+ntdesmond@users.noreply.github.com>
2022-05-20 16:41:36 +0300

    Add type annotation syntax parsing

---
+++ Source/OCompiler/Analyze/Syntax/Declaration/TypeAnnotation.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+
+using OCompiler.Analyze.Lexical.Tokens;
+using OCompiler.Analyze.Lexical.Tokens.Delimiters;
+using OCompiler.Exceptions;
+using OCompiler.Utils;
+
+namespace OCompiler.Analyze.Syntax.Declaration;
+
+internal class TypeAnnotation
+{
+    public Identifier Name { get; }
+    public List<Identifier> GenericTypes { get; } = new();
+
+    public TypeAnnotation(Identifier name)
+    {
+        Name = name;
+    }
+
+    public TypeAnnotation(Identifier name, IEnumerable<Identifier> generics)
+    {
+        Name = name;
+        GenericTypes = new(generics);
+    }
+
+    public static bool TryParse(TokenEnumerator tokens, out TypeAnnotation? type)
+    {
+        if (tokens.Current() is not Identifier name)
+        {
+            type = null;
+            return false;
+        }
+
+        var genericTypes = new List<Identifier>();
+        if (tokens.Next() is LeftAngleBracket)
+        {
+            ParseGenericsList(tokens);
+        }
+
+        type = new TypeAnnotation(name, genericTypes);
+        return true;
+    }
+
+    private static List<Identifier> ParseGenericsList(TokenEnumerator tokens)
+    {
+        var types = new List<Identifier>();
+
+        while (tokens.Current() is not RightAngleBracket)
+        {
+            if (tokens.Next() is not Identifier type)
+            {
+                throw new SyntaxError(tokens.Current().Position, "Expected a type in the generics list");
+            }
+            types.Add(type);
+
+            if (tokens.Next() is not (Comma or RightAngleBracket))
+            {
+                throw new SyntaxError(tokens.Current().Position, "Expected a comma or end of generics list");
+            }
+        }
+
+        return types;
+    }
+}
--- Source/OCompiler/Analyze/Syntax/Declaration/Variable.cs
+++ Source/OCompiler/Analyze/Syntax/Declaration/Variable.cs
@@ -1,14 +1,15 @@
-using System;
 using OCompiler.Analyze.Lexical.Tokens;
-using OCompiler.Analyze.Syntax.Declaration.Statement;
 using OCompiler.Exceptions;
 using OCompiler.Utils;
+
+using System;
 
 namespace OCompiler.Analyze.Syntax.Declaration;
 
 internal class Variable : IBodyStatement
 {
     public Identifier Identifier { get; }
+    public TypeAnnotation? Type { get; }
     public Expression.Expression Expression { get; }
 
     public static bool TryParse(TokenEnumerator tokens, out Variable? variable)
@@ -25,7 +26,15 @@ internal class Variable : IBodyStatement
         {
             throw new SyntaxError(tokens.Current().Position, "Expected variable name");
         }
-        
+
+
+        // Optional type annotation.
+        TypeAnnotation? type = null;
+        if (tokens.Next() is Lexical.Tokens.Delimiters.Colon)
+        {
+            type = ParseTypeAnnotation(tokens);
+        }
+
         // Assign delimiter.
         if (tokens.Next() is not Lexical.Tokens.Delimiters.Equals)
         {
@@ -41,13 +50,24 @@ internal class Variable : IBodyStatement
             throw new SyntaxError(tokens.Current().Position, "Expected expression");
         }
 
-        variable = new Variable(name, expression!);
+        variable = new Variable(name, type, expression!);
         return true;
     }
 
-    protected Variable(Identifier name, Expression.Expression expression)
+    private static TypeAnnotation? ParseTypeAnnotation(TokenEnumerator tokens)
+    {
+        tokens.Next();
+        if (!TypeAnnotation.TryParse(tokens, out var type))
+        {
+            throw new SyntaxError(tokens.Current().Position, "Expected a type");
+        }
+        return type;
+    }
+
+    protected Variable(Identifier name, TypeAnnotation? type, Expression.Expression expression)
     {
         Identifier = name;
+        Type = type;
         Expression = expression;
     }
 
