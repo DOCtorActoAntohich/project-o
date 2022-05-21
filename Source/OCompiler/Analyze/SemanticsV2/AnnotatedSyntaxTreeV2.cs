72a5786a24d4c34b090b1ffe8e6eb9a721f12ef9
DOCtorActoAntohich <fairy666death@mail.ru>
2022-05-21 15:59:08 +0300

    Add GetClass method to unify access to separate dicts

--- Source/OCompiler/Analyze/SemanticsV2/AnnotatedSyntaxTreeV2.cs
+++ Source/OCompiler/Analyze/SemanticsV2/AnnotatedSyntaxTreeV2.cs
@@ -6,6 +6,7 @@ using OCompiler.Analyze.SemanticsV2.Dom.Expression;
 using OCompiler.Analyze.SemanticsV2.Dom.Type;
 using OCompiler.Analyze.SemanticsV2.Dom.Type.Member;
 using OCompiler.Analyze.SemanticsV2.Tree;
+using OCompiler.Exceptions.Semantic;
 
 namespace OCompiler.Analyze.SemanticsV2;
 
@@ -18,5 +19,20 @@ internal class AnnotatedSyntaxTreeV2
     public AnnotatedSyntaxTreeV2(Syntax.Tree syntaxTree)
     {
         BuiltinClasses = new BuiltinClassTree().Classes;
+    }
+
+    public ClassDeclaration GetClass(string name)
+    {
+        if (BuiltinClasses.ContainsKey(name))
+        {
+            return BuiltinClasses[name];
+        }
+
+        if (ParsedClasses.ContainsKey(name))
+        {
+            return ParsedClasses[name];
+        }
+
+        throw new KeyNotFoundException($"The class {name} hasn't been found");
     }
 }
