7fe5df22447b45247789790ee2b745632bcad9d7
DOCtorActoAntohich <fairy666death@mail.ru>
2022-05-21 21:28:56 +0300

    Add creations of empty class declarations

--- Source/OCompiler/Analyze/SemanticsV2/AnnotatedSyntaxTreeV2.cs
+++ Source/OCompiler/Analyze/SemanticsV2/AnnotatedSyntaxTreeV2.cs
@@ -1,17 +1,17 @@
-using System;
 using System.Collections.Generic;
-using System.Reflection;
 using System.Linq;
-using OCompiler.Analyze.SemanticsV2.Dom.Expression;
 using OCompiler.Analyze.SemanticsV2.Dom.Type;
-using OCompiler.Analyze.SemanticsV2.Dom.Type.Member;
 using OCompiler.Analyze.SemanticsV2.Tree;
+using OCompiler.Analyze.Syntax.Declaration;
 using OCompiler.Exceptions.Semantic;
+using ParsedClassData = OCompiler.Analyze.Syntax.Declaration.Class.Class;
 
 namespace OCompiler.Analyze.SemanticsV2;
 
 internal class AnnotatedSyntaxTreeV2
 {
+    private static readonly string RootClassName = "Class";
+    
     public Dictionary<string, ClassDeclaration> BuiltinClasses { get; }
     public Dictionary<string, ClassDeclaration> ParsedClasses { get; } = new();
 
@@ -19,6 +19,8 @@ internal class AnnotatedSyntaxTreeV2
     public AnnotatedSyntaxTreeV2(Syntax.Tree syntaxTree)
     {
         BuiltinClasses = new BuiltinClassTree().Classes;
+        
+        CreateDeclarationsFrom(syntaxTree);
     }
 
     public ClassDeclaration GetClass(string name)
@@ -34,5 +36,46 @@ internal class AnnotatedSyntaxTreeV2
         }
 
         throw new KeyNotFoundException($"The class {name} hasn't been found");
+    }
+
+    public bool HasClass(string name)
+    {
+        return BuiltinClasses.ContainsKey(name) || ParsedClasses.ContainsKey(name);
+    }
+
+    private void CreateDeclarationsFrom(Syntax.Tree syntaxTree)
+    {
+        foreach (var parsedClass in syntaxTree)
+        {
+            var declaration = CreateEmptyDeclaration(parsedClass);
+            AddClassDeclaration(declaration);
+
+            AddGenericTypeReferences(declaration, parsedClass);
+        }
+    }
+
+    private ClassDeclaration CreateEmptyDeclaration(ParsedClassData parsedClass)
+    {
+        if (HasClass(parsedClass.NameLiteral))
+        {
+            throw new NameCollisionError(
+                parsedClass.TokenPosition, $"Name {parsedClass.NameLiteral} is already defined");
+        }
+
+        return new ClassDeclaration(parsedClass.NameLiteral);
+    }
+    
+    private void AddClassDeclaration(ClassDeclaration declaration)
+    {
+        ParsedClasses.Add(declaration.Name, declaration);
+    }
+
+    private static void AddGenericTypeReferences(ClassDeclaration declaration, ParsedClassData parsedClass)
+    {
+        foreach (var genericType in parsedClass.Name.GenericTypes)
+        {
+            var genericTypeReference = new TypeReference(genericType.Name.Literal, isGeneric: true);
+            declaration.GenericTypes.Add(genericTypeReference);
+        }
     }
 }
