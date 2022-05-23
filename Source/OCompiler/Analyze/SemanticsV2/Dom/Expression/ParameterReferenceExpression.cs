47d4cde90e0a0558e1affc96bcd127d52d62e4ee
DOCtorActoAntohich <fairy666death@mail.ru>
2022-05-23 23:30:49 +0300

    Add parameter reference expression

---
+++ Source/OCompiler/Analyze/SemanticsV2/Dom/Expression/ParameterReferenceExpression.cs
@@ -0,0 +1,8 @@
+namespace OCompiler.Analyze.SemanticsV2.Dom.Expression;
+
+internal class ParameterReferenceExpression : Expression
+{
+    public ParameterReferenceExpression(string name) : base(name)
+    {
+    }
+}
