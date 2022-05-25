d8fbb185bb6988e82efc11e75b0394c6b13ea93f
DOCtorActoAntohich <fairy666death@mail.ru>
2022-05-25 18:02:10 +0300

    Create reference to constructor in ObjectCreateExpression

--- Source/OCompiler/Analyze/SemanticsV2/Dom/Expression/Call/ObjectCreateExpression.cs
+++ Source/OCompiler/Analyze/SemanticsV2/Dom/Expression/Call/ObjectCreateExpression.cs
@@ -1,11 +1,14 @@
 using System.Collections.Generic;
 using OCompiler.Analyze.SemanticsV2.Dom.Type;
+using OCompiler.Analyze.SemanticsV2.Dom.Type.Member;
 
 namespace OCompiler.Analyze.SemanticsV2.Dom.Expression.Call;
 
 internal class ObjectCreateExpression : CallExpression
 {
     public List<TypeReference> GenericTypes { get; } = new();
+
+    public MemberConstructor Constructor { get; set; } = null!;
 
     public ObjectCreateExpression(TypeReference type) : base(type.Name)
     {
