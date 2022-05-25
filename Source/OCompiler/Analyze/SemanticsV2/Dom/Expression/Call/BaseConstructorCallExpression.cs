f3d96a83091ca0401402526370382d3be28f563b
DOCtorActoAntohich <fairy666death@mail.ru>
2022-05-25 17:42:37 +0300

    Add reference to called constructor in BaseConstructorCallExpression

--- Source/OCompiler/Analyze/SemanticsV2/Dom/Expression/Call/BaseConstructorCallExpression.cs
+++ Source/OCompiler/Analyze/SemanticsV2/Dom/Expression/Call/BaseConstructorCallExpression.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
+using OCompiler.Analyze.SemanticsV2.Dom.Type.Member;
 using BaseToken = OCompiler.Analyze.Lexical.Tokens.Keywords.Base;
 
 namespace OCompiler.Analyze.SemanticsV2.Dom.Expression.Call;
 
 internal class BaseConstructorCallExpression : CallExpression
 {
+    public MemberConstructor Constructor { get; set; } = null!;
+    
     public BaseConstructorCallExpression() : base(BaseToken.Literal)
     {
     }
