6d6519eb6894d1f9d31fe33f9ae70bf8ed821484
DOCtorActoAntohich <fairy666death@mail.ru>
2022-05-25 23:20:13 +0300

    I sand no more throwing balls

--- Source/OCompiler/Program.cs
+++ Source/OCompiler/Program.cs
@@ -8,9 +8,9 @@ namespace OCompiler
     {
         private static void Main(string[] args)
         {
-                var assembly = new Compiler(sourceFilePath: args[0]).Run();
             try
             {
+                var assembly = new Compiler(sourceFilePath: args[0]).Run();
 
                 Console.WriteLine("Program output:");
                 new Invoker(assembly, args[1], args[2..]).Run();
@@ -18,7 +18,6 @@ namespace OCompiler
             catch (Exception exception)
             {
                 Console.WriteLine($"Exception: {exception.GetType().Name}(\"{exception.Message}\")");
-                throw;
             }
         }
     }
